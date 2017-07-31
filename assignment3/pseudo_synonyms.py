e51d0f98d861576d19511b11e577c8b6e237ff0f
Ori Kalinski <ori@mintigo.com>
2017-07-31 21:43:32 +0300

    small changes to support python3

--- assignment3/pseudo_synonyms.py
+++ assignment3/pseudo_synonyms.py
@@ -8,7 +8,7 @@ SERVER_PASSWORD = "pass"
 
 def map_func_phase_one(k, sentence):
     split_sentence = sentence.split()
-    for i in xrange(len(split_sentence)):
+    for i in range(len(split_sentence)):
         yield " ".join(split_sentence[:i] + split_sentence[i + 1:]), split_sentence[i]
 
 
@@ -18,17 +18,19 @@ def reduce_func_phase_one(k, vs):
 
 
 def map_func_phase_two(k, relevant_words):
-    for word in relevant_words:
-        yield word, 1
+    for i in range(len(relevant_words)):
+        for j in range(i + 1, len(relevant_words)):
+            yield "%s - %s" % (relevant_words[i], relevant_words[j]), 1
 
 
 def reduce_func_phase_two(k, vs):
-    if sum(vs) > 1:
-        return k
+    sum_vs = sum(vs)
+    if sum_vs > 1:
+        return sum_vs
 
 
 def main():
-    with open(DATA_FILE, "rb") as f:
+    with open(DATA_FILE, "r") as f:
         data = map(str.strip, f.readlines())
     s = mincemeat.Server()
     s.mapfn = map_func_phase_one
@@ -41,8 +43,9 @@ def main():
     s.reducefn = reduce_func_phase_two
     s.datasource = dict(enumerate(results))
     results = s.run_server(password=SERVER_PASSWORD, port=SERVER_PORT)
-    results = filter(None, results.values())
-    print "%s (%s)" % (" - ".join(results), len(results))
+    for key, num_of_ref in results.items():
+        if num_of_ref:
+            print("%s (%s)" % (key, num_of_ref))
 
 if __name__ == '__main__':
     main()
